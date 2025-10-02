8cf2f31b74c7999e2ae360529475458da186aa90
Pheonix <149215780+Pheonix-1002@users.noreply.github.com>
2025-10-02 14:38:04 +0530

    Add files via upload

---
+++ AppleRotation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float rotationSpeed=50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime); 
+    }
+}
